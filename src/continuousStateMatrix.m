5bbdf5b4c24b20cca34c812276c2a0108419fd5a
Giuseppe <g.lombardi11@studenti.unipi.it>
2023-10-05 03:19:41 +0200

    2d trial and new matrix hidden weights generation

--- src/continuousStateMatrix.m
+++ src/continuousStateMatrix.m
@@ -1,10 +1,17 @@
 function W_hat = continuousStateMatrix(Nh, eigenvalues, seed)
 rng(seed)
 
-D = diag(eigenvalues);
+%D = diag(eigenvalues);
+r = 1;
+theta = pi/2;
+block = r*[cos(theta), sin(theta); -sin(theta), cos(theta)];
+cell_block = repmat({block}, 1, Nh/2);
+D = blkdiag(cell_block{:});
 V = rand(Nh); 
-W_hat = V * (D / V); %W_hat = V*(D*inv(V))
-%W_hat = V \ (D*V); %W_hat = inv(V)*(D*V)
+W_hat = V * (D / V);
+W_hat=D;
+
+%W_hat = V - V';
 
 end
 
